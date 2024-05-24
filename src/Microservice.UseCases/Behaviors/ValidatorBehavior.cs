14045b0dbc6c14602d115b9c381678e15941188f
Ahmed Mohamed El Ahmar <ahmedmohamedelahmar@gmail.com>
2024-05-24 19:27:15 +0100

    refactor: use Count instead of Any().

--- src/Microservice.UseCases/Behaviors/ValidatorBehavior.cs
+++ src/Microservice.UseCases/Behaviors/ValidatorBehavior.cs
@@ -6,18 +6,21 @@ using Microsoft.Extensions.Logging;
 
 namespace Microservice.UseCases.Behaviors;
 
-public class ValidatorBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+public class ValidatorBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
 {
     private readonly ILogger<ValidatorBehavior<TRequest, TResponse>> _logger;
     private readonly IEnumerable<IValidator<TRequest>> _validators;
 
-    public ValidatorBehavior(ILogger<ValidatorBehavior<TRequest, TResponse>> logger, IEnumerable<IValidator<TRequest>> validators)
+    public ValidatorBehavior(ILogger<ValidatorBehavior<TRequest, TResponse>> logger,
+        IEnumerable<IValidator<TRequest>> validators)
     {
         _logger = logger;
         _validators = validators;
     }
 
-    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
     {
         var typeName = request.GetGenericTypeName();
 
@@ -29,14 +32,13 @@ public class ValidatorBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest
             .Where(error => error != null)
             .ToList();
 
-        if (failures.Any())
-        {
-            _logger.LogWarning("Validation errors - {CommandType} - Command: {@Command} - Errors: {@ValidationErrors}", typeName, request, failures);
+        if (failures.Count == 0) return await next();
 
-            throw new OrderingDomainException(
-                $"Command Validation Errors for type {typeof(TRequest).Name}", new ValidationException("Validation exception", failures));
-        }
+        _logger.LogWarning("Validation errors - {CommandType} - Command: {@Command} - Errors: {@ValidationErrors}",
+            typeName, request, failures);
 
-        return await next();
+        throw new OrderingDomainException(
+            $"Command Validation Errors for type {typeof(TRequest).Name}",
+            new ValidationException("Validation exception", failures));
     }
 }
