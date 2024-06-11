6df8ee80672100460071a3c313536200cb9be4f0
Ahmed Mohamed El Ahmar <ahmedmohamedelahmar@gmail.com>
2024-06-11 20:59:35 +0100

    tests: domain events should be sealed. tests: domain events should have DomainEvent postfix.

---
+++ tests/Microservice.ArchTests/Domain/DomainTests.cs
@@ -0,0 +1,33 @@
+using ArchUnitNET.Fluent;
+using ArchUnitNET.xUnit;
+using MediatR;
+
+namespace Microservice.ArchTests.Domain;
+
+public class DomainTests : ArchUnitBaseTest
+{
+
+    [Fact]
+    public void DomainEventsShouldBeSealed()
+    {
+        ArchRuleDefinition
+            .Classes()
+            .That()
+            .ImplementInterface(typeof(INotification))
+            .Should()
+            .BeSealed()
+            .Check(Architecture);
+    }
+    
+    [Fact]
+    public void DomainEventsShouldHaveDomainEventPostfix()
+    {
+        ArchRuleDefinition
+            .Classes()
+            .That()
+            .ImplementInterface(typeof(INotification))
+            .Should()
+            .HaveNameEndingWith("DomainEvent")
+            .Check(Architecture);
+    }
+}
